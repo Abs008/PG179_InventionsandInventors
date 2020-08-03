64dfe1931eeb23ace6d2b075591c23fab87d6d56
Abhay <abhaysinghroyale.6@gmail.com>
2020-08-03 20:14:09 +0530

    update

--- app/src/main/java/com/example/dg_ghats/FeedbackActivity.java
+++ app/src/main/java/com/example/dg_ghats/FeedbackActivity.java
@@ -1,50 +1,73 @@
 package com.example.dg_ghats;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class FeedbackActivity extends AppCompatActivity {
-    private Button hotelbtn;
-    EditText namedata,emaildata,messagedata;
-    Button details, submit;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button chosefile , upload ;
+    ImageView imgforup;
+    ProgressBar progbar;
+    EditText filedata,emaildata,messagedata;
     Firebase firebase;
+    private Uri mImageUri;
+
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
-        namedata=findViewById(R.id.namedata);
+
+        filedata=findViewById(R.id.filedata);
         emaildata=findViewById(R.id.emaildata);
         messagedata=findViewById(R.id.messagedata);
+        imgforup=findViewById(R.id.imgforup);
+        progbar=findViewById(R.id.progbar);
 
+        upload = findViewById(R.id.upload);
+        chosefile= findViewById(R.id.chosefile);
 
-
-        submit = findViewById(R.id.submit);
-        details= findViewById(R.id.details);
         Firebase.setAndroidContext(this);
 
-        hotelbtn = findViewById(R.id.hotelbtn);
 
-        hotelbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent categoryIntent = new Intent(FeedbackActivity.this,HotelActivity.class);
-                startActivity(categoryIntent);
-                finish();
-            }
-        });
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploads");
+
+
 
         String UniqueID =
          Settings.Secure.getString(getApplicationContext().getContentResolver() ,
@@ -52,64 +75,109 @@ public class FeedbackActivity extends AppCompatActivity {
 
         firebase = new Firebase("https://d-g--ghats.firebaseio.com/Users" + UniqueID);
 
-        submit.setOnClickListener(new View.OnClickListener() {
+        chosefile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                details.setEnabled(true);
-                final String name = namedata.getText().toString();
+                openFileChoser();
+            }
+
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadFile();
                 final String email = emaildata.getText().toString();
                 final String message = messagedata.getText().toString();
 
 
-                Firebase child_name = firebase.child("Name");
-                child_name.setValue(name);
-                if (name.isEmpty()){
-                    namedata.setError("Required Field");
-                    submit.setEnabled(false);
-                }else
-                {
-                    namedata.setError(null);
-                    submit.setEnabled(true);
-                }
-
                 Firebase child_email = firebase.child("Email");
                 child_email.setValue(email);
-                if (email.isEmpty())
-                {
+                if (email.isEmpty()) {
                     emaildata.setError("Required Field");
-                    submit.setEnabled(false);
-                }else
-                {
+                    upload.setEnabled(false);
+                } else {
                     emaildata.setError(null);
-                    submit.setEnabled(true);
+                    upload.setEnabled(true);
                 }
 
                 Firebase child_message = firebase.child("Message");
                 child_message.setValue(message);
-                if (message.isEmpty())
-                {
+                if (message.isEmpty()) {
                     messagedata.setError("Required Field");
-                    submit.setEnabled(false);
-                }else
-                {
+                    upload.setEnabled(false);
+                } else {
                     messagedata.setError(null);
-                    submit.setEnabled(true);
+                    upload.setEnabled(true);
                 }
-                Toast.makeText(FeedbackActivity.this, "Your feedback is recorded " , Toast.LENGTH_SHORT).show();
-
-                details.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        new AlertDialog.Builder(FeedbackActivity.this)
-                                .setTitle("Sent Details")
-                                .setMessage("Name -" + name +"\n\nEmail-" + email + "\n\nMessage -" + message)
-                                .show();
-                    }
-                });
+                Toast.makeText(FeedbackActivity.this, "Your feedback is recorded ", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
+
+
+    private void openFileChoser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent , PICK_IMAGE_REQUEST);
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mimeTypeMap =MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+
+    private void uploadFile() {
+        if (mImageUri != null){
+            StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+            + "." + getFileExtension(mImageUri));
+
+            FileDownloadTask.TaskSnapshot taskSnapshot;
+            StorageTask<UploadTask.TaskSnapshot> taskSnapshotStorageTask = fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(FeedbackActivity.this, e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot){
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            progbar.setProgress((int)progress);
+
+                        }
+                    });
+        }else{
+            Toast.makeText(this,"No file",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && requestCode == RESULT_OK
+        && data != null && data.getData() != null){
+            mImageUri = data.getData();
+
+            Picasso.with(this).load(mImageUri).into(imgforup);
+
+        }
+    }
+
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(FeedbackActivity.this,Homeactivity.class);
